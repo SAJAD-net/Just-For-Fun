d12708b9425155f3dc08a9499bbdf3bce6c4150e
sajad-net <sajadsajad.sch@gmail.com>
2021-02-09 04:38:00 -0500

    changed a app

--- Fibonachi/fibonachi.go
+++ Fibonachi/fibonachi.go
@@ -2,12 +2,9 @@ package main
 
 import "fmt"
 
-
 func main(){
-
-	var first, second, fib int = 0,1,0
-	var item int
-	for item=0; item <= 10; item+=1{
+	first, second, fib  := 0,1,0
+	for item:=0; item <= 10; item+=1{
 		fmt.Printf("[-]- This is [FIRST] number --> %d\n", first)
 		fmt.Printf("[-]- This is [SECOND] number --> %d\n", second)
 		fib = first+second
